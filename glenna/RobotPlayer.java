4d6bca4ecb945356c35f782ca3cc5bab8fe616c1
swablulu <glz6@cornell.edu>
2017-01-13 17:06:30 -0500

    Plant trees in a circle rather than randomly

--- glenna/RobotPlayer.java
+++ glenna/RobotPlayer.java
@@ -110,7 +110,7 @@ public strictfp class RobotPlayer {
                 }
                 
                 //Try planting a tree, else wander
-                if (!tryPlantTree(randomDirection(), 45, 4) && !rc.hasMoved()) 
+                if (!tryPlantTree(new Direction(0), 60, 3) && !rc.hasMoved()) 
                 	wander();
                 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
